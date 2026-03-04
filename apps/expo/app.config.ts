ab3fdb07fd9ea1d29f8503c6396359b4a1464b21
Matt Calayo <matthewcalayo@gmail.com>
2026-03-04 13:02:17 -0500

    packages upgraded, expo 55

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -12,7 +12,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   updates: {
     fallbackToCacheTimeout: 0,
   },
-  newArchEnabled: true,
   assetBundlePatterns: ["**/*"],
   ios: {
     bundleIdentifier: "your.bundle.identifier",
@@ -28,7 +27,6 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
       foregroundImage: "./assets/icon-light.png",
       backgroundColor: "#1F104A",
     },
-    edgeToEdgeEnabled: true,
   },
   // extra: {
   //   eas: {
