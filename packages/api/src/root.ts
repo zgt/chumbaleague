5dd22fbe0b8127cac5e95184867e193dde29c2f0
Matt Calayo <matthewcalayo@gmail.com>
2026-03-04 13:43:23 -0500

    api core

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,8 +1,10 @@
 import { authRouter } from "./router/auth";
+import { musicLeagueRouter } from "./router/music-league";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  musicLeague: musicLeagueRouter,
 });
 
 // export type definition of API
